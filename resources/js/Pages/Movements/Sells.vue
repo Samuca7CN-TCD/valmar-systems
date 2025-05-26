2daf3ecf75f299d82452b120f00eba7e4395b376
Samuca7CN-TCD <samuca.7cn@gmail.com>
2025-05-26 16:19:04 -0300

    AJuste na pagina de vendas

--- resources/js/Pages/Movements/Sells.vue
+++ resources/js/Pages/Movements/Sells.vue
@@ -1,174 +1,174 @@
 <script setup>
-import AppLayout from '@/Layouts/AppLayout.vue'
-import CreateUpdateSellsModal from '@/Components/Movements/CreateUpdateSellsModal.vue'
-import FloatButton from '@/Components/FloatButton.vue'
-import ExtraOptionsButton from '@/Components/ExtraOptionsButton.vue'
-import { Head, useForm } from '@inertiajs/vue3'
-import { computed, ref } from 'vue'
-import { ChevronDownIcon, ChevronUpIcon, InformationCircleIcon, EyeIcon, MagnifyingGlassIcon, PencilIcon, XMarkIcon } from '@heroicons/vue/24/outline'
-import { toMoney, formatDate, calcDeadlineDays } from '@/general.js'
-import { useToast } from 'vue-toast-notification';
-import 'vue-toast-notification/dist/theme-sugar.css';
-// =============================================
-// Informações exteriores
-const props = defineProps({
-    page: Object,
-    page_options: {
-        type: Array,
-        default: null,
-    },
-    sells_list: Object,
-    items: Object,
-    parameters: Object,
-})
-const $toast = useToast();
+    import AppLayout from '@/Layouts/AppLayout.vue'
+    import CreateUpdateSellsModal from '@/Components/Movements/CreateUpdateSellsModal.vue'
+    import FloatButton from '@/Components/FloatButton.vue'
+    import ExtraOptionsButton from '@/Components/ExtraOptionsButton.vue'
+    import { Head, useForm } from '@inertiajs/vue3'
+    import { computed, ref } from 'vue'
+    import { ChevronDownIcon, ChevronUpIcon, InformationCircleIcon, EyeIcon, MagnifyingGlassIcon, PencilIcon, XMarkIcon } from '@heroicons/vue/24/outline'
+    import { toMoney, formatDate, calcDeadlineDays } from '@/general.js'
+    import { useToast } from 'vue-toast-notification';
+    import 'vue-toast-notification/dist/theme-sugar.css';
+    // =============================================
+    // Informações exteriores
+    const props = defineProps({
+        page: Object,
+        page_options: {
+            type: Array,
+            default: null,
+        },
+        sells_list: Object,
+        items: Object,
+        parameters: Object,
+    })
+    const $toast = useToast();
 
-// =============================================
-// Informações do OBJETO
-const sell_data = useForm({
-    'id': null,
-    'client': '',
-    'date': formatDate(),
-    'estimated_value': 0,
-    'total_value': 0,
-    'partial_value': 0,
-    'discount_percent': 0,
-    'discount': 0,
-    'entry_value': 0,
-    'observations': '',
-    'items_list': [],
-})
+    // =============================================
+    // Informações do OBJETO
+    const sell_data = useForm({
+        'id': null,
+        'client': '',
+        'date': formatDate(),
+        'estimated_value': 0,
+        'total_value': 0,
+        'partial_value': 0,
+        'discount_percent': 0,
+        'discount': 0,
+        'entry_value': 0,
+        'observations': '',
+        'items_list': [],
+    })
 
-const sell_filter_toggle = ref(false)
-const sell_filter = useForm({
-    entity_name: props.parameters.entity_name,
-    start_date: props.parameters.start_date,
-    end_date: props.parameters.end_date,
-})
+    const sell_filter_toggle = ref(false)
+    const sell_filter = useForm({
+        entity_name: props.parameters.entity_name,
+        start_date: props.parameters.start_date,
+        end_date: props.parameters.end_date,
+    })
 
-const search_term = ref("")
+    const search_term = ref("")
 
-const filtered_sells_list = computed(() => {
-    const searchTermLower = search_term.value.toLowerCase();
-    return props.sells_list.filter(el => {
-        const { id, entity_name, observations, date, accounting, items } = el;
-        const { total_value, partial_value } = accounting;
-        const fieldsToCheck = [
-            id.toString(),
-            entity_name?.toLowerCase() ?? "",
-            observations?.toLowerCase() ?? "",
-            date?.toLowerCase() ?? "",
-            toMoney(total_value).toLowerCase() ?? "",
-            toMoney(partial_value).toLowerCase() ?? ""
-        ];
-        for (let field of fieldsToCheck) {
-            if (field.includes(searchTermLower)) {
-                return true;
+    const filtered_sells_list = computed(() => {
+        const searchTermLower = search_term.value.toLowerCase();
+        return props.sells_list.filter(el => {
+            const { id, entity_name, observations, date, accounting, items } = el;
+            const { total_value, partial_value } = accounting;
+            const fieldsToCheck = [
+                id.toString(),
+                entity_name?.toLowerCase() ?? "",
+                observations?.toLowerCase() ?? "",
+                date?.toLowerCase() ?? "",
+                toMoney(total_value)?.toLowerCase() ?? "",
+                toMoney(partial_value)?.toLowerCase() ?? ""
+            ];
+            for (let field of fieldsToCheck) {
+                if (field.includes(searchTermLower)) {
+                    return true;
+                }
+            }
+            return items.some(item => item.name.toLowerCase().includes(searchTermLower));
+        });
+    });
+
+
+    const total_sells_amount = computed(() => {
+        return props.sells_list.reduce((accumulator, sell) => {
+            return accumulator + sell.accounting.partial_value;
+        }, 0);
+    });
+    // =============================================
+    // Controle de Modal
+    const modal = ref({
+        mode: 'create',
+        show: false,
+        get title() {
+            switch (this.mode) {
+                case 'create': return "Criar venda"
+                case 'update': return "Editar venda"
+                case 'see': return "Ver informações da venda"
+            }
+        },
+        get primary_button_txt() {
+            switch (this.mode) {
+                case 'create': return "Cadastrar"
+                case 'update': return "Atualizar"
+                case 'see': return "Fechar"
             }
         }
-        return items.some(item => item.name.toLowerCase().includes(searchTermLower));
-    });
-});
-
-
-const total_sells_amount = computed(() => {
-    return props.sells_list.reduce((accumulator, sell) => {
-        return accumulator + sell.accounting.partial_value;
-    }, 0);
-});
-// =============================================
-// Controle de Modal
-const modal = ref({
-    mode: 'create',
-    show: false,
-    get title() {
-        switch (this.mode) {
-            case 'create': return "Criar venda"
-            case 'update': return "Editar venda"
-            case 'see': return "Ver informações da venda"
-        }
-    },
-    get primary_button_txt() {
-        switch (this.mode) {
-            case 'create': return "Cadastrar"
-            case 'update': return "Atualizar"
-            case 'see': return "Fechar"
-        }
-    }
-})
-
-const openModal = (mode, sell_id = null) => {
-    const isUpdateOrSeeMode = ['update', 'see'].includes(mode);
-    if (sell_id !== null && isUpdateOrSeeMode) {
-        const sell = props.sells_list.find(sell => sell.id === sell_id);
-        if (sell) {
-            const { id, entity_name, date, observations, accounting, items } = sell;
-
-            sell_data.id = id;
-            sell_data.client = entity_name;
-            sell_data.date = date;
-            sell_data.estimated_value = accounting.estimated_value;
-            sell_data.total_value = accounting.total_value;
-            sell_data.partial_value = accounting.partial_value;
-            sell_data.entry_value = 0;
-            sell_data.discount = accounting.estimated_value - accounting.total_value;
-            sell_data.discount_percent = (sell_data.discount / (accounting.estimated_value || 1)) * 100;
-            sell_data.observations = observations;
-            sell_data.items_list = items
-        }
-    }
-    modal.value.mode = mode;
-    modal.value.show = true;
-};
-
-
-const closeModal = () => {
-    sell_data.reset()
-    modal.value.show = false
-}
-
-
-// =============================================
-// Métodos de CRUD
-const createSell = () => {
-    sell_data.post(route('sells.store'), {
-        preserveScroll: true,
-        onSuccess: () => closeModal(),
-        onError: (error) => { console.log(error) }
     })
-}
 
-const get_filtered_data = () => {
-    sell_filter.post(route('sells.filter'), {
-        preserveScroll: true,
-    });
-};
+    const openModal = (mode, sell_id = null) => {
+        const isUpdateOrSeeMode = ['update', 'see'].includes(mode);
+        if (sell_id !== null && isUpdateOrSeeMode) {
+            const sell = props.sells_list.find(sell => sell.id === sell_id);
+            if (sell) {
+                const { id, entity_name, date, observations, accounting, items } = sell;
 
-const updateSell = () => {
-    sell_data.put(route('sells.update', sell_data.id), {
-        preserveScroll: true,
-        onSuccess: () => closeModal()
-    })
-}
+                sell_data.id = id;
+                sell_data.client = entity_name;
+                sell_data.date = date;
+                sell_data.estimated_value = accounting.estimated_value;
+                sell_data.total_value = accounting.total_value;
+                sell_data.partial_value = accounting.partial_value;
+                sell_data.entry_value = 0;
+                sell_data.discount = accounting.estimated_value - accounting.total_value;
+                sell_data.discount_percent = (sell_data.discount / (accounting.estimated_value || 1)) * 100;
+                sell_data.observations = observations;
+                sell_data.items_list = items
+            }
+        }
+        modal.value.mode = mode;
+        modal.value.show = true;
+    };
 
 
-const deleteSell = (sell_id, sell_name) => {
-    if (confirm(`Você tem certeza que deseja excluir a venda de material para "${sell_name}"? Todos os materiais comprados voltarão para o estoque! Esta ação não poderá ser desfeita!`)) {
-        sell_data.delete(route('sells.destroy', sell_id), {
+    const closeModal = () => {
+        sell_data.reset()
+        modal.value.show = false
+    }
+
+
+    // =============================================
+    // Métodos de CRUD
+    const createSell = () => {
+        sell_data.post(route('sells.store'), {
             preserveScroll: true,
-            onSuccess: () => $toast.success('Venda deletado com sucesso!')
+            onSuccess: () => closeModal(),
+            onError: (error) => { console.log(error) }
         })
     }
-}
 
-const submit = () => {
-    switch (modal.value.mode) {
-        case 'create': return createSell()
-        case 'update': return updateSell()
-        case 'see': return closeModal()
-        default: $toast.error('Método desconhecido. Informar o Técnico.')
+    const get_filtered_data = () => {
+        sell_filter.post(route('sells.filter'), {
+            preserveScroll: true,
+        });
+    };
+
+    const updateSell = () => {
+        sell_data.put(route('sells.update', sell_data.id), {
+            preserveScroll: true,
+            onSuccess: () => closeModal()
+        })
     }
-}
+
+
+    const deleteSell = (sell_id, sell_name) => {
+        if (confirm(`Você tem certeza que deseja excluir a venda de material para "${sell_name}"? Todos os materiais comprados voltarão para o estoque! Esta ação não poderá ser desfeita!`)) {
+            sell_data.delete(route('sells.destroy', sell_id), {
+                preserveScroll: true,
+                onSuccess: () => $toast.success('Venda deletado com sucesso!')
+            })
+        }
+    }
+
+    const submit = () => {
+        switch (modal.value.mode) {
+            case 'create': return createSell()
+            case 'update': return updateSell()
+            case 'see': return closeModal()
+            default: $toast.error('Método desconhecido. Informar o Técnico.')
+        }
+    }
 </script>
 
 <template>
@@ -178,7 +178,7 @@ const submit = () => {
         <template class="print:hidden" #header>
             <h2 class="font-semibold text-xl text-gray-800 leading-tight">
                 {{ page.name }} <span v-if="$page.props.auth.user.hierarchy < 2">| {{ toMoney(total_sells_amount)
-                    }}</span>
+                }}</span>
             </h2>
             <FloatButton :icon="'plus'" @click="openModal('create')" title="Cadastrar Sell" class="print:hidden" />
         </template>
@@ -214,7 +214,7 @@ const submit = () => {
                                     class="simple-input disabled:bg-gray-200" placeholder="Filtrar por nome do cliente"
                                     v-model="sell_filter.entity_name" @input="get_filtered_data" required>
                                 <p v-if="sell_filter.errors.entity_name" class="text-red-500 text-sm">{{
-        sell_filter.errors.entity_name }}</p>
+                                    sell_filter.errors.entity_name }}</p>
                             </div>
                         </div>
 
@@ -227,7 +227,7 @@ const submit = () => {
                                     class="simple-input disabled:bg-gray-200" placeholder="Filtrar por período (início)"
                                     v-model="sell_filter.start_date" @input="get_filtered_data" required>
                                 <p v-if="sell_filter.errors.start_date" class="text-red-500 text-sm">{{
-        sell_filter.errors.start_date }}</p>
+                                    sell_filter.errors.start_date }}</p>
                             </div>
                         </div>
 
@@ -240,7 +240,7 @@ const submit = () => {
                                     class="simple-input disabled:bg-gray-200" placeholder="Filtrar por período (fim)"
                                     v-model="sell_filter.end_date" @input="get_filtered_data" required>
                                 <p v-if="sell_filter.errors.end_date" class="text-red-500 text-sm">{{
-        sell_filter.errors.end_date }}</p>
+                                    sell_filter.errors.end_date }}</p>
                             </div>
                         </div>
 
@@ -291,18 +291,18 @@ const submit = () => {
                                                     v-for="sell in filtered_sells_list"
                                                     class="border-b transition duration-300 ease-in-out hover:bg-neutral-100 print:break-inside-avoid">
                                                     <td class="whitespace-nowrap py-4 text-center font-medium">{{
-        sell.id }}
+                                                        sell.id }}
                                                     </td>
                                                     <td class="whitespace-normal px-6 py-4 text-center trim">{{
-        sell.entity_name }}
+                                                        sell.entity_name }}
                                                         <span v-if="sell.observations" :title="sell.observations"
                                                             class="inline-flex items-center rounded-md bg-gray-50 px-2 py-1 text-xs font-medium text-gray-600 ring-1 ring-inset ring-gray-500/10 cursor-help">OBS</span>
                                                     </td>
                                                     <td class="whitespace-nowrap px-2 py-4 text-center">{{
-        toMoney(sell.accounting.partial_value) }}</td>
+                                                        toMoney(sell.accounting.partial_value) }}</td>
                                                     <td class="whitespace-nowrap px-2 py-4 text-center">{{
-        sell.accounting.total_value > 0 ?
-            toMoney(sell.accounting.total_value) : 'Pago' }}</td>
+                                                        sell.accounting.total_value > 0 ?
+                                                            toMoney(sell.accounting.total_value) : 'Pago' }}</td>
                                                     <td class="whitespace-nowrap px-2 py-4 text-center">{{
                                                         formatDate(sell.date, true) }}</td>
                                                     <!--
